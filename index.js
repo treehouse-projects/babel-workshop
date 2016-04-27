9a5cbd04dae0e38995d8cad867ef451dc43f56b5
Isaac Lee Morris <isaaclee@ilmatic.io>
2016-04-27 07:47:38 -0700

    add babel-cli template

--- index.js
+++
@@ -1,3 +0,0 @@
-var myArrowFunction = () => "Hello, Arrow Functions.";
-
-console.log(myArrowFunction());
---
+++ src/index.js
@@ -0,0 +1,4 @@
+var double = (value) => value * 2;
+
+console.log("What is 1337 x 2?");
+console.log(double(1337));
